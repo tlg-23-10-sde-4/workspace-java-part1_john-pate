3675a861a09fbdd4ace17ebc0adb62294ee2a330
John Pate <john.pate@tlgcohort.com>
2023-10-24 14:10:07 -0500

    Lab08.2 (Inheritance) complete

--- Lab08.2/src/com/hr/personnel/client/HRClient.java
+++ Lab08.2/src/com/hr/personnel/client/HRClient.java
@@ -10,6 +10,9 @@ package com.hr.personnel.client;
 
 import com.hr.personnel.Department;
 import com.hr.personnel.Employee;
+import com.hr.personnel.HourlyEmployee;
+import com.hr.personnel.SalariedEmployee;
+
 import java.time.LocalDate;
 
 /**
@@ -25,6 +28,8 @@ class HRClient {
         // add Employees to it
         dept.addEmployee(new Employee("Jason", LocalDate.of(1990, 8, 24)));
         dept.addEmployee(new Employee("Julie", LocalDate.of(2000, 2, 2)));
+        dept.addEmployee(new SalariedEmployee("Jungus", LocalDate.of(1995, 6, 10), 140000));
+        dept.addEmployee(new HourlyEmployee("Jubilant", LocalDate.of(1989, 11, 10), 32, 42));
 
         // list its Employees
         System.out.println("\nList employees:");
