897e59aaa810f8c8c060fb92d87f72272ee7ed63
John Pate <john.pate@tlgcohort.com>
2023-10-26 11:39:58 -0500

    Lab10.1-Television complete

---
+++ Lab10.1-Television/src/com/entertainment/DisplayType.java
@@ -0,0 +1,12 @@
+package com.entertainment;/*
+ * There are 5 instances of DisplayType, listed below. NOT strings.
+ * There are 6 DisplayType objects are referenced as:
+ *  DisplayType.LED
+ *  DisplayType.OLED
+ *  [enum].values
+ *  [enum].valueOf
+ */
+
+public enum DisplayType {
+    LED, OLED, PLASMA, LCD, CRT
+}
---
+++ Lab10.1-Television/src/com/entertainment/InvalidBrandException.java
@@ -0,0 +1,21 @@
+package com.entertainment;
+
+// CHECKED exception, because it extends Exception
+public class InvalidBrandException extends Exception {
+
+    public InvalidBrandException() {
+        super();
+    }
+
+    public InvalidBrandException(String message) {
+        super(message);
+    }
+
+    public InvalidBrandException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidBrandException(Throwable cause) {
+        super(cause);
+    }
+}
