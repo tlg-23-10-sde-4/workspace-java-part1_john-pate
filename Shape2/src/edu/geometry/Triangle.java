b30eec18ed7c3f48fa1a323a4f086f66546d1cfe
John Pate <john.pate@tlgcohort.com>
2023-10-30 15:50:27 -0500

    Shape - triangle area completion

--- Shape2/src/edu/geometry/Triangle.java
+++ Shape2/src/edu/geometry/Triangle.java
@@ -1,9 +1,29 @@
 package edu.geometry;
 
-class Triangle {
+public class Triangle implements Shape {
     private double side1;
     private double side2;
     private double angle;
-    // TODO: Side-Angle-Side
 
+    public Triangle (double side1, double side2, double angle) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.angle = angle;
+    }
+
+    public double area () {
+        return ((getSide1() * getSide2()) * Math.sin(getAngle())) / 2;
+    }
+
+    public double getSide1() {
+        return side1;
+    }
+
+    public double getSide2() {
+        return side2;
+    }
+
+    public double getAngle() {
+        return angle * (Math.PI/180);
+    }
 }
--- Shape2/src/edu/geometry/client/ShapeClient.java
+++ Shape2/src/edu/geometry/client/ShapeClient.java
@@ -3,16 +3,20 @@ package edu.geometry.client;
 import edu.geometry.Circle;
 import edu.geometry.Rectangle;
 import edu.geometry.Shape;
+import edu.geometry.Triangle;
 
 class ShapeClient {
 
     public static void main(String[] args) {
-        Shape[] shapes = new Shape[4];
+        Shape[] shapes = new Shape[6];
         shapes[0] = new Circle(2.5);
         shapes[1] = new Circle(3.2);
         System.out.println();
         shapes[2] = new Rectangle(2.0, 3.0);
         shapes[3] = new Rectangle(2.0, 4.0);
+        System.out.println();
+        shapes[4] = new Triangle(5.0, 5.0, 30.0);
+        shapes[5] = new Triangle(5.0, 6.0, 42.0);
 
         for (Shape shape : shapes) {
             System.out.printf("The area of the %s is %s\n", shape.getClass().getSimpleName(), shape.area());
